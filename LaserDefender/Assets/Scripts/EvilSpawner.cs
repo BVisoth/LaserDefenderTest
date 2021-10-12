7b6f6917557537d268585a09c3ea286fd2ecee92
BVisoth <thefracturedowl@gmail.com>
2021-10-12 13:53:29 -0700

    Added more codeing

--- LaserDefender/Assets/Scripts/EvilSpawner.cs
+++ LaserDefender/Assets/Scripts/EvilSpawner.cs
@@ -6,23 +6,38 @@ public class EvilSpawner : MonoBehaviour
 {
 
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEvilInWave(currentWave));
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            StartCoroutine(SpawnAllEvilInWave(currentWave));
+        }
+        return null;
     }
 
     private IEnumerator SpawnAllEvilInWave(WaveConfig waveConfig)
     {
         for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
         {
-            Instantiate(
+            var newEnemy = Instantiate(
              waveConfig.GetEnemyPrefab(),
              waveConfig.GetWaypoints()[0].transform.position,
              Quaternion.identity);
+            newEnemy.GetComponent<EvilPath>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
         }
     }
